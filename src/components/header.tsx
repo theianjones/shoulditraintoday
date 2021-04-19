5b7076db5c3e4ea007558ab7f5ebe21d10ecc02c
Zac Jones <zacjones93@gmail.com>
2021-04-19 18:28:19 -0400

    update header justify-between

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
+import DarkModeToggle from './dark-mode-toggle'
+
 function Header() {
   return (
     <div className="relative bg-white-50 dark:bg-gray-800">
-      <div className="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10 w-full">
-        <div>
+      <div className="flex justify-between items-center px-4 py-6 sm:px-6">
+        <div className="flex">
           <a
             href="#"
             className="flex items-center font-bold dark:text-gray-50 text-gray-800"
@@ -17,7 +19,7 @@ function Header() {
           </a>
         </div>
 
-        <div className="hidden md:flex md:items-center md:justify-between">
+        <div className="hidden md:flex md:items-end md:justify-between">
           <div className="flex items-center md:ml-12">
             <a
               href="#"
@@ -27,11 +29,12 @@ function Header() {
             </a>
             <a
               href="#"
-              className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+              className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-700 hover:bg-red-800"
             >
               Sign up
             </a>
           </div>
+          <DarkModeToggle />
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Head from 'next/head'
-import DarkModeToggle from '../components/dark-mode-toggle'
+
 import Header from 'components/header'
 export default function Home() {
   return (
     <div className="dark:bg-gray-800 bg-gray-50">
       <Head>
-        <title>Next.js, TypeScript, Tailwind, Jest</title>
+        <title>shoulditrain.today</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <DarkModeToggle />
     </div>
   )
 }
