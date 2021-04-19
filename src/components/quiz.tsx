cc3aab6d29ad6326e64ece05e301562e69cffa2c
Zac Jones <zacjones93@gmail.com>
2021-04-19 19:39:58 -0400

    start quiz component

--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,6 +1,7 @@
+import {FunctionComponent} from 'react'
+
 function quiz() {
-  let question = 'How many days in a row have you trained?'
-  let questionAnswer = {
+  let quiz = {
     question: 'How many days in a row have you trained?',
     answers: [
       {
@@ -27,140 +28,56 @@ function quiz() {
   }
 
   return (
-    <div className="grid grid-cols-6 gap-4">
-      <h1 className="text-4xl leading-10 font-extrabold text-center col-start-2 col-span-4">
-        {question}
+    <div className="grid grid-cols-6 gap-4 mt-8">
+      <h1 className="text-4xl leading-10 font-extrabold text-center col-start-2 col-span-4 mb-4">
+        {quiz.question}
       </h1>
       <fieldset className="col-start-2 col-span-4">
         <legend className="sr-only">Server size</legend>
         <div className="space-y-4">
-          <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-            <input
-              type="radio"
-              name="server_size"
-              value="Hobby"
-              className="sr-only"
-              aria-labelledby="server-size-0-label"
-              aria-describedby="server-size-0-description-0 server-size-0-description-1"
-            />
-            <div className="flex items-center">
-              <div className="text-3xl">
-                <p
-                  id="server-size-0-label"
-                  className="font-light text-gray-900"
-                >
-                  4+ days
-                </p>
-              </div>
-            </div>
-          </label>
-
-          <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-            <input
-              type="radio"
-              name="server_size"
-              value="Startup"
-              className="sr-only"
-              aria-labelledby="server-size-1-label"
-              aria-describedby="server-size-1-description-0 server-size-1-description-1"
-            />
-            <div className="flex items-center">
-              <div className="text-3xl">
-                <p
-                  id="server-size-0-label"
-                  className="font-light text-gray-900"
-                >
-                  3 days
-                </p>
-              </div>
-            </div>
-            <div
-              className="border-transparent absolute -inset-px rounded-lg border-2 pointer-events-none"
-              aria-hidden="true"
-            ></div>
-          </label>
-
-          <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-            <input
-              type="radio"
-              name="server_size"
-              value="Business"
-              className="sr-only"
-              aria-labelledby="server-size-2-label"
-              aria-describedby="server-size-2-description-0 server-size-2-description-1"
-            />
-            <div className="flex items-center">
-              <div className="text-3xl">
-                <p
-                  id="server-size-0-label"
-                  className="font-light text-gray-900"
-                >
-                  2 days
-                </p>
-              </div>
-            </div>
-            <div
-              id="server-size-2-description-1"
-              className="mt-2 flex text-sm sm:mt-0 sm:block sm:ml-4 sm:text-right"
-            ></div>
-            <div
-              className="border-transparent absolute -inset-px rounded-lg border-2 pointer-events-none"
-              aria-hidden="true"
-            ></div>
-          </label>
-
-          <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-            <input
-              type="radio"
-              name="server_size"
-              value="Enterprise"
-              className="sr-only"
-              aria-labelledby="server-size-3-label"
-              aria-describedby="server-size-3-description-0 server-size-3-description-1"
-            />
-            <div className="flex items-center">
-              <div className="text-3xl">
-                <p
-                  id="server-size-0-label"
-                  className="font-light text-gray-900"
-                >
-                  1 days
-                </p>
-              </div>
-            </div>
-            <div
-              className="border-transparent absolute -inset-px rounded-lg border-2 pointer-events-none"
-              aria-hidden="true"
-            ></div>
-          </label>
-
-          <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-            <input
-              type="radio"
-              name="server_size"
-              value="Enterprise"
-              className="sr-only"
-              aria-labelledby="server-size-3-label"
-              aria-describedby="server-size-3-description-0 server-size-3-description-1"
-            />
-            <div className="flex items-center">
-              <div className="text-3xl">
-                <p
-                  id="server-size-0-label"
-                  className="font-light text-gray-900"
-                >
-                  Coming off a rest day
-                </p>
-              </div>
-            </div>
-            <div
-              id="server-size-3-description-1"
-              className="mt-2 flex text-sm sm:mt-0 sm:block sm:ml-4 sm:text-right"
-            ></div>
-          </label>
+          {quiz.answers.map((option) => (
+            <Option option={option.option} />
+          ))}
         </div>
       </fieldset>
+      <Button className="col-start-2 col-span-4 mt-10">Next Question</Button>
     </div>
+  )
+}
+
+let Option = ({option}: any) => {
+  return (
+    <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+      <input
+        type="radio"
+        name="server_size"
+        value="Hobby"
+        className="sr-only"
+        aria-labelledby="server-size-0-label"
+      />
+      <div className="flex items-center">
+        <div className="text-3xl">
+          <p id="server-size-0-label" className="font-light text-gray-900">
+            {option}
+          </p>
+        </div>
+      </div>
+    </label>
+  )
+}
+
+let Button: FunctionComponent<{
+  className?: string
+}> = ({children, className}) => {
+  return (
+    <button
+      type="button"
+      className={`p-6 border border-transparent text-3xl text-center font-medium rounded-md shadow-sm text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 ${
+        className ? className : ''
+      }`}
+    >
+      {children}
+    </button>
   )
 }
 
