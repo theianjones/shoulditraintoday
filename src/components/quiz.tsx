e9fe4c10f3b95397a373329c6b4db3ce9b770114
ijones16 <jones58ian@gmail.com>
2021-04-19 20:39:23 -0400

    add formik

--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,64 +1,79 @@
+import React, {FormEvent, SyntheticEvent} from 'react'
 import {FunctionComponent} from 'react'
+import {Formik, Field, Form} from 'formik'
 
-function quiz() {
-  let quiz = {
-    question: 'How many days in a row have you trained?',
-    answers: [
-      {
-        option: '4+ days',
-        value: 1,
-      },
-      {
-        option: '3 days',
-        value: 2,
-      },
-      {
-        option: '2 days',
-        value: 3,
-      },
-      {
-        option: '1 day',
-        value: 4,
-      },
-      {
-        option: 'Coming off a rest day',
-        value: 5,
-      },
-    ],
-  }
+let quiz = {
+  question: 'How many days in a row have you trained?',
+  answers: [
+    {
+      option: '4+ days',
+      value: '1',
+    },
+    {
+      option: '3 days',
+      value: '2',
+    },
+    {
+      option: '2 days',
+      value: '3',
+    },
+    {
+      option: '1 day',
+      value: '4',
+    },
+    {
+      option: 'Coming off a rest day',
+      value: '5',
+    },
+  ],
+}
 
+function Quiz({name = 'Quiz'}) {
   return (
     <div className="grid grid-cols-6 gap-4 mt-8">
       <h1 className="text-4xl leading-10 font-extrabold text-center col-start-2 col-span-4 mb-4">
         {quiz.question}
       </h1>
-      <fieldset className="col-start-2 col-span-4">
-        <legend className="sr-only">Server size</legend>
-        <div className="space-y-4">
-          {quiz.answers.map((option) => (
-            <Option option={option.option} />
-          ))}
-        </div>
-      </fieldset>
-      <Button className="col-start-2 col-span-4 mt-10">Next Question</Button>
+      <Formik
+        initialValues={{[name]: ''}}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500))
+          alert(JSON.stringify(values, null, 2))
+        }}
+      >
+        {({values}) => (
+          <Form className="col-start-2 col-span-4">
+            <legend className="sr-only">Server size</legend>
+            <div className="space-y-4">
+              {quiz.answers.map((option) => (
+                <Option label={option.option} key={option.value}>
+                  <Field
+                    type="radio"
+                    name={name}
+                    value={option.value}
+                    checked={values[name] === option.value}
+                  />
+                </Option>
+              ))}
+            </div>
+            <Button className="col-start-2 col-span-4 mt-10">
+              Next Question
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
 
-let Option = ({option}: any) => {
+let Option = ({children, label}: any) => {
   return (
     <label className="relative block rounded-lg border border-gray-300 bg-white shadow-sm px-6 py-4 cursor-pointer hover:border-gray-400 sm:flex sm:justify-between focus-within:ring-1 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-      <input
-        type="radio"
-        name="server_size"
-        value="Hobby"
-        className="sr-only"
-        aria-labelledby="server-size-0-label"
-      />
+      {children}
       <div className="flex items-center">
         <div className="text-3xl">
           <p id="server-size-0-label" className="font-light text-gray-900">
-            {option}
+            {label}
           </p>
         </div>
       </div>
@@ -81,4 +96,4 @@ let Button: FunctionComponent<{
   )
 }
 
-export default quiz
+export default Quiz
